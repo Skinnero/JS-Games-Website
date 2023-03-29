565336173b78472a5376ccd12f843bbfe9c35a49
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2023-03-29 11:34:29 +0200

    Adding snake from project

--- data_handler/data_handler.py
+++
@@ -1,8 +0,0 @@
-from data_handler.connection import CURSOR
-
-def save_score_to_db(data:dict) -> None:
-
-    data = [str(v) for v in data.values()]
-    print(data)
-    query = "INSERT INTO scoreboard(user_name, game, score) VALUES (%s, %s, %s)"
-    CURSOR.execute(query,data)
